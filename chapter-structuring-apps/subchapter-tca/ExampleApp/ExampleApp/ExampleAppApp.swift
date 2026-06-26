44c3a6c042bff55b4ac4160b0e58cbb7c2833073
Natascha Fadeeva <natascha@appocados.com>
2026-06-26 19:38:15 +0200

    Add new project for tca sample code

---
+++ chapter-structuring-apps/subchapter-tca/ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Natascha Fadeeva on 26.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
